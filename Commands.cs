a9a9afc663076fe8987d37f31c28b39b54ed8772
Trainfire <aarongarcha@gmail.com>
2018-09-12 20:49:51 +0200

    Setup basic bot
    Nu kör vi!

---
+++ Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace svenskabot
+{
+    public class Commands
+    {
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"Tjena, {ctx.User.Mention}!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace svenskabot
+{
+    class Config
+    {
+        public string Token { get; set; }
+    }
+
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+
+        static void Main(string[] args)
+        {
+            MainAsync(GetToken()).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string token)
+        {
+            discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!",
+            });
+
+            commands.RegisterCommands<Commands>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        static string GetToken()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory + "token";
+
+            if (File.Exists(path))
+            {
+                using (var file = File.OpenText(path))
+                {
+                    var token = file.ReadToEnd();
+                    Console.WriteLine("Token was loaded from file.");
+                    return token;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a token:");
+
+                var token = Console.ReadLine();
+
+                using (var sw = File.CreateText(path))
+                {
+                    sw.Write(token);
+                }
+
+                Console.WriteLine("Token was saved to file.");
+
+                return token;
+            }
+        }
+    }
+}
