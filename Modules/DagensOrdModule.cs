5ed5cbb9ba8f41b131d35b7ff90d3a28cc3d05e8
Trainfire <aarongarcha@gmail.com>
2021-05-23 20:25:53 +0200

    Change grodan day to Friday.

--- Modules/DagensOrdModule.cs
+++ Modules/DagensOrdModule.cs
@@ -106,7 +106,7 @@ namespace svenskabot
 
                         Entry.AddToBuilder(embed);
 
-                        if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+                        if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
                         {
                             try
                             {
